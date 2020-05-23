599e658441d3870ce413778a3b204f17e79acd70
Erhan Arda <erhanarda7@gmail.com>
2020-05-23 16:01:43 +0300

    HomePage hooks added

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,146 +1,128 @@
-import React, { Component } from 'react'
+import React, { useState,useEffect} from 'react'
 import { connect } from "react-redux"
 import { Books, FilterSortBar } from "../../components"
 import { getBooks } from "../../state/ducks/books/actions"
-import { Container, Pagination, PaginationItem, PaginationLink,ButtonGroup, Button } from 'reactstrap'
+import { Container, Pagination, PaginationItem, PaginationLink, ButtonGroup, Button } from 'reactstrap'
 import { itemsPerPage } from "../../constants"
 
-export class HomePage extends Component {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props)
-        this.state = {
-            activePage: 1,
-            view: "cards"
-        }
+
+const HomePage = (props) => {
+
+    const [activePage, setActivePage] = useState(1)
+    const [view, setView] = useState("cards")
+    useEffect(() => {
+        props.getBooks()
+    }, [])
+
+
+
+    if (props.loading) {
+        return <h1>Books are loading...</h1>
+    }
+    //console.log("filter",this.props.filter)
+    const allBooks = props.books;
+    let filteredBooks = []
+    // filter for category
+    if (props.filter) {
+        filteredBooks = allBooks.filter((book) => {
+            return book.category === props.filter
+        })
+    } else {
+        filteredBooks = allBooks
     }
 
-    componentDidMount() {
-        this.props.getBooks()
+    //filter for searchTerm
+    if (props.searchTerm) {
+        filteredBooks = filteredBooks.filter((book) => {
+            return book.title.toLowerCase().indexOf(props.searchTerm.toLowerCase()) > -1 || book.author.toLowerCase().indexOf(props.searchTerm.toLowerCase()) > -1
+
+        })
+    }
+
+    //Sorting
+    let sortedBooks = [];
+    if (props.sortTerm) {
+        sortedBooks = [...filteredBooks].sort((book1, book2) => {
+            return parseInt(book2[props.sortTerm]) - parseInt(book1[props.sortTerm])
+        })
+    } else {
+        sortedBooks = filteredBooks
     }
 
 
-    render() {
-        if (this.props.loading) {
-            return <h1>Books are loading...</h1>
-        }
-        //console.log("filter",this.props.filter)
-        const allBooks = this.props.books;
-        let filteredBooks = []
-        // filter for category
-        if (this.props.filter) {
-            filteredBooks = allBooks.filter((book) => {
-                return book.category === this.props.filter
-            })
-        } else {
-            filteredBooks = allBooks
-        }
-
-        //filter for searchTerm
-        if (this.props.searchTerm) {
-            filteredBooks = filteredBooks.filter((book) => {
-                return book.title.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) > -1 || book.author.toLowerCase().indexOf(this.props.searchTerm.toLowerCase()) > -1
-
-            })
-        }
-
-        //Sorting
-        let sortedBooks = [];
-        if (this.props.sortTerm) {
-            sortedBooks = [...filteredBooks].sort((book1, book2) => {
-                return parseInt(book2[this.props.sortTerm]) - parseInt(book1[this.props.sortTerm])
-            })
-        } else {
-            sortedBooks = filteredBooks
-        }
-
-
-        let books = "";
-        if (sortedBooks.length < 1) {
-            books = <h2>NO BOOK HERE</h2>
-        }
-        else {
-            books = <Books view={this.state.view} items={sortedBooks.slice(itemsPerPage * (this.state.activePage - 1), itemsPerPage * this.state.activePage)} />
-
-        }
-        console.log("pages: ", Math.ceil(sortedBooks.length / itemsPerPage))
-        const PaginationArray = new Array(Math.ceil(sortedBooks.length / itemsPerPage))
-        PaginationArray.fill(null)
-        return (
-            <div>
-                <FilterSortBar />
-                <Container>
-                    <div>
-                        <ButtonGroup>
-                            <Button color="primary" onClick={()=>{
-                                this.setState({
-                                    view: "cards"
-                                })
-                            }} >Cards View</Button>
-                            <Button color="primary"onClick={()=>{
-                                this.setState({
-                                    view: "list"
-                                })
-                            }} >List View</Button>
-                        </ButtonGroup>
-                    </div>
-
-                    <div className="books-holder">
-                        {books}
-                    </div>
-
-                    <Pagination aria-label="Page navigation example">
-                        <PaginationItem disabled={this.state.activePage === 1}>
-                            <PaginationLink first href="#" onClick={(e) => {
-                                e.preventDefault()
-                                this.setState({
-                                    activePage: 1
-                                })
-                            }} />
-                        </PaginationItem>
-                        <PaginationItem disabled={this.state.activePage === 1}>
-                            <PaginationLink previous href="#" onClick={(e) => {
-                                e.preventDefault()
-                                this.setState({
-                                    activePage: this.state.activePage - 1
-                                })
-                            }} />
-                        </PaginationItem>
-                        {
-                            PaginationArray.map((page, index) => {
-                                const pageNum = index + 1
-                                return <PaginationItem active={pageNum === this.state.activePage}>
-                                    <PaginationLink href="#" onClick={(e) => {
-                                        e.preventDefault()
-                                        this.setState({ activePage: pageNum })
-                                    }} >
-                                        {pageNum}
-                                    </PaginationLink>
-                                </PaginationItem>
-                            })
-                        }
-                        <PaginationItem disabled={this.state.activePage === PaginationArray.length}>
-                            <PaginationLink next href="#" onClick={(e) => {
-                                e.preventDefault()
-                                this.setState({
-                                    activePage: this.state.activePage + 1
-                                })
-                            }} />
-                        </PaginationItem>
-                        <PaginationItem disabled={this.state.activePage === PaginationArray.length}>
-                            <PaginationLink last href="#" onClick={() => {
-                                this.setState({
-                                    activePage: PaginationArray.length
-                                })
-                            }} />
-                        </PaginationItem>
-                    </Pagination>
-                </Container>
-            </div>
-        )
+    let books = "";
+    if (sortedBooks.length < 1) {
+        books = <h2>NO BOOK HERE</h2>
     }
+    else {
+        books = <Books view={view} items={sortedBooks.slice(itemsPerPage * (activePage - 1), itemsPerPage * activePage)} />
+
+    }
+    console.log("pages: ", Math.ceil(sortedBooks.length / itemsPerPage))
+    const PaginationArray = new Array(Math.ceil(sortedBooks.length / itemsPerPage))
+    PaginationArray.fill(null)
+    return (
+        <div>
+            <FilterSortBar />
+            <Container>
+                <div>
+                    <ButtonGroup>
+                        <Button color="primary" onClick={() => {
+                            setView("cards")
+                        }} >Cards View</Button>
+                        <Button color="primary" onClick={() => {
+                            setView("list")
+                        }} >List View</Button>
+                    </ButtonGroup>
+                </div>
+
+                <div className="books-holder">
+                    {books}
+                </div>
+
+                <Pagination aria-label="Page navigation example">
+                    <PaginationItem disabled={activePage === 1}>
+                        <PaginationLink first href="#" onClick={(e) => {
+                            e.preventDefault()
+                            setActivePage(1)
+                        }} />
+                    </PaginationItem>
+                    <PaginationItem disabled={activePage === 1}>
+                        <PaginationLink previous href="#" onClick={(e) => {
+                            e.preventDefault()
+                            setActivePage(activePage - 1)
+                        }} />
+                    </PaginationItem>
+                    {
+                        PaginationArray.map((page, index) => {
+                            const pageNum = index + 1
+                            return <PaginationItem active={pageNum === activePage}>
+                                <PaginationLink href="#" onClick={(e) => {
+                                    e.preventDefault()
+                                    setActivePage(pageNum)
+                                }} >
+                                    {pageNum}
+                                </PaginationLink>
+                            </PaginationItem>
+                        })
+                    }
+                    <PaginationItem disabled={activePage === PaginationArray.length}>
+                        <PaginationLink next href="#" onClick={(e) => {
+                            e.preventDefault()
+                            setActivePage(activePage - 1)
+                        }} />
+                    </PaginationItem>
+                    <PaginationItem disabled={activePage === PaginationArray.length}>
+                        <PaginationLink last href="#" onClick={() => {
+                            
+                            setActivePage(PaginationArray.length)
+                        }} />
+                    </PaginationItem>
+                </Pagination>
+            </Container>
+        </div>
+    )
 }
-
 
 
 const mapStateToProps = (state) => {
